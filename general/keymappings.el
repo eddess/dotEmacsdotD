9f46f15d0913df9efbb14df1afd9b2f3130d2a6f
Eddy Essien <eddy.essien@gmail.com>
2014-01-01 15:16:55 -0600

    Fix global indentation

--- general/keymappings.el
+++ general/keymappings.el
@@ -4,9 +4,9 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 ;; indentation
-(setq indent-tabs-mode t)
-(setq tab-width 4)
-(setq tab-stop-list (number-sequence 4 160 4))
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4)
+(setq-default tab-stop-list (number-sequence 4 160 4))
 (define-key text-mode-map (kbd "<tab>") 'tab-to-tab-stop)
 
 ;; package variable
