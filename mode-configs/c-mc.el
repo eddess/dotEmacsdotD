23a68fd6afecd80d5077574916ec613c03c7a530
Eddy Essien <eddy.essien@gmail.com>
2014-01-28 08:07:18 +0000

    Some keymaping stuff

---
+++ mode-configs/c-mc.el
@@ -0,0 +1,18 @@
+(defun my-c-mode-common-hook ()
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
+
+										;(setq c++-tab-always-indent t)
+  (setq c-basic-offset 4)                  ;; Default is 2
+  (setq c-indent-level 4)                  ;; Default is 2
+
+  (setq-default tab-stop-list (number-sequence 4 160 4))
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)  ; use spaces only if nil
+  )
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+;; package variable
+(provide 'c-mc)
