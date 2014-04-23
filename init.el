0ee5b96c9e1bc04cb1014180389806e1b4a83409
Eddy Essien <eddy.essien@gmail.com>
2014-04-23 01:18:50 +0100

    NEw stuff

--- init.el
+++ init.el
@@ -1,12 +1,24 @@
+
+;;; Function for path on OSX
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 ;;; default directory
 (setq default-directory "~/")
 
 ;; custom file
 (setq custom-file "~/.emacs.d/customizations.el")
 
-;; load-path customizations
-(add-to-list 'load-path "~/.emacs.d/general")
-(add-to-list 'load-path "~/.emacs.d/mode-configs")
+;; load path
+(add-to-list 'load-path "~/.emacs.d/configs")
 (add-to-list 'load-path "~/.emacs.d/themes")
 
 ;; info path
@@ -16,15 +28,11 @@
 
 ;; load packages
 (dolist (file '(core
-				appearance
-				keymappings
-
-				;; utility configs
-				ssh-config
-				spell-mc
-				
-				;; File configs
-				php-mc
-				tex-mc
-				c-mc))
+		appearance
+		keymappings
+		
+		php-mc
+		tex-mc
+		c-mc
+		spell-mc))
   (require file))
