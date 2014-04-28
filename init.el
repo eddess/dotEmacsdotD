eeb98dd5a0a5cf03cea23038cdccbad3e6563116
Eddy Essien <eddy.essien@gmail.com>
2014-04-28 09:51:37 +0100

    Nice state

--- init.el
+++ init.el
@@ -1,21 +1,16 @@
 
-;;; Function for path on OSX
-(defun set-exec-path-from-shell-PATH ()
-  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-
-This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-  (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(set-exec-path-from-shell-PATH)
-
 ;;; default directory
 (setq default-directory "~/")
 
 ;; custom file
 (setq custom-file "~/.emacs.d/customizations.el")
+
+;; package management
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
 
 ;; load path
 (add-to-list 'load-path "~/.emacs.d/configs")
@@ -24,15 +19,15 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 ;; info path
 (setq Info-default-directory-list
       (append Info-default-directory-list
-	      '("~/.emacs.d/info")))
+			  '("~/.emacs.d/info")))
 
 ;; load packages
 (dolist (file '(core
-		appearance
-		keymappings
+				appearance
+				keymappings
 		
-		php-mc
-		tex-mc
-		c-mc
-		spell-mc))
+				php-mc
+				tex-mc
+				c-mc
+				spell-mc))
   (require file))
