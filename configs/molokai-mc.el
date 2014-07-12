8648cc7bf735285c59bc66432529251557089a0b
Eddy Essien <eessien@coverity.com>
2014-07-12 11:57:47 -0600

    Molokai theme with modified modeline colours

--- configs/molokai-mc.el
+++ configs/molokai-mc.el
@@ -1,4 +1,13 @@
 (require 'molokai-theme)
+ 
+;; modeline colors
+(custom-set-faces
+ '(mode-line ((t (:foreground "#f9f9f9" :background "grey20" :box nil))))
+ '(mode-line-buffer-id ((t (:foreground "#f9f9f9" :background "grey20" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "grey30" :box nil)))))
+
+; Set cursor color to white
+(add-to-list 'default-frame-alist '(cursor-color . "white"))
 
 
 (provide 'molokai-mc)
