8c66a744f0bc5c1e5cffb29f1d1cc62f5e9f93ee
Eddy Essien <eessien@coverity.com>
2014-08-29 17:08:02 -0600

    Hidden modeline and flycheck faces in basic theme

--- configs/basic-theme-mc.el
+++ configs/basic-theme-mc.el
@@ -3,27 +3,31 @@
 (let ((bt-blue "#3535AE")
 
 	  (bt-red "#940000")
-	  
+	  (bt-red-error "#E90000")
+
       (bt-gray "#909090")
 	  (bt-gray-lightest "#E0E0E0")
 	  (bt-gray-light "#C9C9C9")
-	  
+
 	  (bt-green "#004000")
-      
+
       (bt-magenta "#9C2073")
 
 	  (bt-purple "#3B006F")
 
-	  (bt-yellow "#7A3100")
+	  (bt-brown "#7A3100")
+
+	  (bt-orange-warning "#FF9100")
 
 	  (bt-cyan "#006565")
+	  (bt-cyan-info "#16E9E9")
 
 	  (bt-white "#FFFFFF")
 
 	  (bt-black "#000000")
 
 	  (bt-fg "#000000")
-	  
+
 	  (bt-bg "#FFFFFF"))
 
   ;; modeline colors
@@ -34,8 +38,8 @@
   (custom-set-faces
    ;; Fringe
    `(fringe ((t (:background ,bt-bg))))
- 
-   ;; basic faces 
+
+   ;; basic faces
    `(default ((t (:foreground ,bt-fg :background ,bt-bg))))
    `(cursor ((t (:foreground ,bt-bg))))
    `(minibuffer-prompt ((t (:foreground ,bt-blue :weight Bold))))
@@ -44,14 +48,14 @@
    ;; Main
    `(font-lock-builtin-face ((t (:foreground ,bt-green :weight Bold))))
    `(font-lock-comment-face ((t (:foreground ,bt-gray))))
-   `(font-lock-constant-face ((t (:foreground ,bt-yellow))))
+   `(font-lock-constant-face ((t (:foreground ,bt-brown))))
    `(font-lock-doc-string-face ((t (:foreground ,bt-gray-light))))
    `(font-lock-function-name-face ((t (:foreground ,bt-green))))
    `(font-lock-keyword-face ((t (:foreground ,bt-red :weight Bold))))
-   `(font-lock-string-face ((t (:foreground ,bt-yellow))))
+   `(font-lock-string-face ((t (:foreground ,bt-brown))))
    `(font-lock-type-face ((t (:foreground ,bt-blue))))
    `(font-lock-variable-name-face ((t (:foreground ,bt-magenta))))
-   `(font-lock-warning-face ((t (:bold t :foreground ,bt-yellow))))
+   `(font-lock-warning-face ((t (:bold t :foreground ,bt-brown))))
 
    ;; IDO
    `(ido-first-match ((t (:foreground ,bt-purple :weight Bold))))
@@ -62,13 +66,18 @@
    `(term-color-black ((t (:foreground ,bt-bg :weight Bold))))
    `(term-color-red ((t (:foreground ,bt-red :weight Bold))))
    `(term-color-green ((t (:foreground ,bt-green :weight Bold))))
-   `(term-color-yellow ((t (:foreground ,bt-yellow :weight Bold))))
+   `(term-color-yellow ((t (:foreground ,bt-brown :weight Bold))))
    `(term-color-blue ((t (:foreground ,bt-blue :weight Bold))))
    `(term-color-magenta ((t (:foreground ,bt-magenta :weight Bold))))
    `(term-color-cyan ((t (:foreground ,bt-cyan :weight Bold))))
    `(term-color-white ((t (:foreground ,bt-fg :weight Bold))))
    '(term-default-fg-color ((t (:inherit term-color-black :weight Bold))))
    '(term-default-bg-color ((t (:inherit term-color-white :weight Bold))))
+
+   ;; Flycheck
+   `(flycheck-error ((t (:foreground ,bt-red-error :weight Bold))))
+   `(flycheck-warning ((t (:foreground ,bt-orange-warning :weight Bold))))
+   `(flycheck-info ((t (:foreground ,bt-cyan-info :weight Bold))))
   ))
 
 (provide 'basic-theme-mc)
