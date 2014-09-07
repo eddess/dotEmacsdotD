1ea85155a0dbe1928fad95713f127bf2bdb2dba2
Eddy Essien <eddy.essien@gmail.com>
2014-09-07 09:00:12 +0100

    Completion key binding

--- configs/completion-mc.el
+++ configs/completion-mc.el
@@ -4,4 +4,7 @@
 ;; very litte delay. start completing right away
 (setq-default company-idle-delay 0.1)
 
+;; force auto completion with key binding
+(global-set-key (kbd "C-<tab>") 'company-complete-common)
+
 (provide 'completion-mc)
