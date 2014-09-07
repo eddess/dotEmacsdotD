aee34c51960e1e6a257f667fc5b5ac9640a4fef9
Eddy Essien <eessien@coverity.com>
2014-09-07 00:14:46 -0600

    Drop marmalade

--- init.el
+++ init.el
@@ -13,7 +13,6 @@
 ;; package management
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 ;; ensure packages
